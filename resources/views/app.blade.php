855613f379f3abc600c42d59fee0740a697d6be2
Al019 <143259763+Al019@users.noreply.github.com>
2025-02-21 15:58:41 +0800

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,6 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    al
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="/images/logo.png" type="image/x-icon" />
